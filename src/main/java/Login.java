312e77d04145314739d3cb25e03d1aee40a1dbb6
Ferhat Altınışık <ferhat1982@gmail.com>
2023-07-03 20:24:39 +0100

    Login ekranlarinin ozelliklerini tamamladim

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Login {
+public class Login {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 23;
+
+        int sum = x + y;
+        System.out.println("sonuc " + sum);
+
+        int mult = x*y;
+
+    }
+
+
 }
